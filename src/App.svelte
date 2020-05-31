8444e080cf864ea94f400af1e3a2fbcbfc5463f8
eudangeld <dan@dopaminamob.com.br>
2020-05-30 23:23:16 -0300

    First release : )

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+  function decrement() {
+    count -= 1;
+  }
+</script>
+
+<style>
+  .positiVo {
+    color: #000000;
+  }
+  .negaAtiva {
+    color: #ff3e00;
+  }
+</style>
+
+<button on:click={increment}>INCREMENTAR</button>
+<p class={count > 0 ? 'positiVo' : 'negaAtiva'}>{count}</p>
+<button on:click={decrement}>DECREMENTAR</button>
